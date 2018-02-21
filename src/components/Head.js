610a5d57dbdc33ace3c5c5822d93e0febffe6645
Leandro Kimura <leandro.kimura@mirumagency.com>
2018-02-21 16:41:21 -0300

    Removing unused code.

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,14 +1,13 @@
 import NextHead from 'next/head'
 
 const Head = () => {
-    return (
-        <div>
-            <div className="test-kimura"></div>
-            <NextHead>
-                <title>My page title</title>
-                <meta name="description" content="Teste test teste"/>
-            </NextHead>
-        </div>
-    )
+  return (
+    <div>
+      <NextHead>
+        <title>My page title</title>
+        <meta name="description" content="Teste test teste"/>
+      </NextHead>
+    </div>
+  )
 }
 export default Head
--- src/next.config.js
+++ src/next.config.js
@@ -1,46 +1,41 @@
 const path = require('path');
 const glob = require('glob');
-// const withSass = require('@zeit/next-sass')
-// module.exports = withSass()
-
-
-
 
 module.exports = {
-    webpack: (config) => {
-        config.module.rules.push(
-            {
-                test: /\.(css|scss)/,
-                loader: 'emit-file-loader',
-                options: {
-                    name: 'dist/[path][name].[ext]'
-                }
-            }
-            ,
-            {
-                test: /\.css$/,
-                use: ['babel-loader', 'raw-loader', 'postcss-loader']
-            }
-            ,
-            {
-                test: /\.s(a|c)ss$/,
-                use: ['babel-loader', 'raw-loader', 'postcss-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            includePaths: ['styles', 'node_modules']
+  webpack: (config) => {
+    config.module.rules.push(
+      {
+        test: /\.(css|scss)/,
+        loader: 'emit-file-loader',
+        options: {
+          name: 'dist/[path][name].[ext]'
+        }
+      }
+      ,
+      {
+        test: /\.css$/,
+        use: ['babel-loader', 'raw-loader', 'postcss-loader']
+      }
+      ,
+      {
+        test: /\.s(a|c)ss$/,
+        use: ['babel-loader', 'raw-loader', 'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['styles', 'node_modules']
 
-                        }
-                    }
-                ]
             }
-        );
-        return config;
-    },
-    exportPathMap() {
-        return {
-            '/': { page: '/' }
-        };
-    },
-    distDir: '../build'
+          }
+        ]
+      }
+    );
+    return config;
+  },
+  exportPathMap() {
+    return {
+      '/': {page: '/'}
+    };
+  },
+  distDir: '../build'
 };
